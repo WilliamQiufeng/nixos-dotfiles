35518ef328b043c7c7cd0cac5f482f0b567859a0
WilliamQiufeng <williamqiufeng@outlook.com>
2026-01-24 20:52:25 +0000

    steam

--- configuration.nix
+++ configuration.nix
@@ -51,6 +51,14 @@ in
 
   programs.niri.enable = true;
 
+  # ----- Steam -----
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
