e3fb3283f97d9d659575f8bc9bc2950f90768622
WilliamQiufeng <williamqiufeng@outlook.com>
2026-01-25 01:31:29 +0000

    Fix https://github.com/univrsal/input-overlay/issues/356

--- config/obs/config.nix
+++ config/obs/config.nix
@@ -6,6 +6,11 @@
     recursive = true;
   };
 
+  # input-overlay folder is not created by default,
+  # which would result in a crash!
+  # see https://github.com/univrsal/input-overlay/issues/356
+  home.file.".config/obs-studio/plugin_config/input-overlay/.keep".text = "";
+
   programs.obs-studio = {
     enable = true;
     plugins = with pkgs.obs-studio-plugins; [
