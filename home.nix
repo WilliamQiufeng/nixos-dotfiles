c47302b50b638ae0ae3bc534ccd3afa88f8d1587
WilliamQiufeng <williamqiufeng@outlook.com>
2026-01-25 01:52:47 +0000

    steam fix

--- home.nix
+++ home.nix
@@ -98,6 +98,27 @@
     ];
   };
 
+  xdg.desktopEntries = {
+    steam = {
+      name = "Steam";
+      exec = "steam %U";
+      icon = "steam";
+      terminal = false;
+      categories = [
+        "Network"
+        "FileTransfer"
+        "Game"
+      ];
+      mimeType = [
+        "x-scheme-handler/steam"
+        "x-scheme-handler/steamlink"
+      ];
+      settings = {
+        PrefersNonDefaultGPU = "false";
+      };
+    };
+  };
+
   services.swayidle.enable = true; # idle management daemon
   services.polkit-gnome.enable = true; # polkit
 
