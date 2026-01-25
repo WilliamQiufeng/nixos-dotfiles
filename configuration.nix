40c8e9179e61e344b0452b15b667b289a8638e41
WilliamQiufeng <williamqiufeng@outlook.com>
2026-01-25 01:24:39 +0000

    update pipewire latency

--- configuration.nix
+++ configuration.nix
@@ -89,6 +89,14 @@ in
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 1024;
+        "default.clock.min-quantum" = 512;
+        "default.clock.max-quantum" = 2048;
+      };
+    };
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
