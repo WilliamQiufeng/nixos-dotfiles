252839bcf5e353d910f9ba82a53c45dd3f2511cd
WilliamQiufeng <williamqiufeng@outlook.com>
2026-01-26 09:49:55 +0000

    set up c dev env

---
+++ projects/c/flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "C/C++ Dev Environment";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+
+      # Define the VSCode extension list
+      # We prefer Clangd for intellisense but keep C/C++ tools for debugging
+      vscode-extensions = with pkgs.vscode-extensions; [
+        ms-vscode.cpptools
+        llvm-vs-code-extensions.vscode-clangd
+        ms-vscode.cmake-tools
+        twxs.cmake
+      ];
+      vscode-with-extensions = pkgs.vscode-with-extensions.override {
+        vscodeExtensions = vscode-extensions;
+        vscode = pkgs.vscode;
+      };
+      code-wrapper = pkgs.writeShellScriptBin "code" ''
+        exec ${vscode-with-extensions}/bin/code \
+          --user-data-dir "$PWD/.vscode/user-data" \
+          "$@"
+      '';
+
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          cmake
+          gnumake
+          ninja
+          gcc
+          clang-tools
+          gdb
+          code-wrapper
+        ];
+
+        # This tells nix-ld where to find the libraries ONLY for this project
+        shellHook = "";
+      };
+    };
+}
