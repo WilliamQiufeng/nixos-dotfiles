3272d0126d9254ec58bd24b3d77a8ce13253c476
WilliamQiufeng <williamqiufeng@outlook.com>
2026-05-15 16:43:15 +0100

    fcitx config migration

--- packages/ime.nix
+++ packages/ime.nix
@@ -52,6 +52,111 @@
             Layout = "";
           };
         };
+
+        addons.pinyin = {
+          globalSection = {
+            ShuangpinProfile = "Ziranma";
+            ShowShuangpinMode = "True";
+            PageSize = "7";
+            SpellEnabled = "True";
+            SymbolsEnabled = "True";
+            ChaiziEnabled = "True";
+            ExtBEnabled = "True";
+            StrokeCandidateEnabled = "True";
+            CloudPinyinEnabled = "False";
+            CloudPinyinIndex = "2";
+            CloudPinyinAnimation = "True";
+            KeepCloudPinyinPlaceHolder = "False";
+            PreeditMode = "Composing pinyin";
+            PreeditCursorPositionAtBeginning = "True";
+            PinyinInPreedit = "False";
+            Prediction = "False";
+            PredictionSize = "49";
+            BackspaceBehaviorOnPrediction = "Backspace when not using on-screen keyboard";
+            SwitchInputMethodBehavior = "Commit current preedit";
+            UseKeypadAsSelection = "False";
+            BackSpaceToUnselect = "True";
+            "Number of sentence" = "2";
+            WordCandidateLimit = "15";
+            LongWordLengthLimit = "4";
+            QuickPhraseKey = "semicolon";
+            VAsQuickphrase = "True";
+            FirstRun = "False";
+          };
+
+          sections = {
+            ForgetWord."0" = "Control+7";
+
+            PrevPage = {
+              "0" = "minus";
+              "1" = "Up";
+              "2" = "KP_Up";
+              "3" = "Page_Up";
+            };
+
+            NextPage = {
+              "0" = "equal";
+              "1" = "Down";
+              "2" = "KP_Down";
+              "3" = "Next";
+            };
+
+            PrevCandidate."0" = "Shift+Tab";
+            NextCandidate."0" = "Tab";
+
+            CurrentCandidate = {
+              "0" = "space";
+              "1" = "KP_Space";
+            };
+
+            CommitRawInput = {
+              "0" = "Return";
+              "1" = "KP_Enter";
+              "2" = "Control+Return";
+              "3" = "Control+KP_Enter";
+              "4" = "Shift+Return";
+              "5" = "Shift+KP_Enter";
+              "6" = "Control+Shift+Return";
+              "7" = "Control+Shift+KP_Enter";
+            };
+
+            ChooseCharFromPhrase = {
+              "0" = "bracketleft";
+              "1" = "bracketright";
+            };
+
+            FilterByStroke."0" = "grave";
+
+            QuickPhraseTriggerRegex = {
+              "0" = ".(/|@)$";
+              "1" = "^(www|bbs|forum|mail|bbs)\\\\.";
+              "2" = "^(http|https|ftp|telnet|mailto):";
+            };
+
+            Fuzzy = {
+              VE_UE = "True";
+              NG_GN = "True";
+              Inner = "True";
+              InnerShort = "True";
+              PartialFinal = "True";
+              PartialSp = "False";
+              V_U = "True";
+              AN_ANG = "True";
+              EN_ENG = "True";
+              IAN_IANG = "True";
+              IN_ING = "True";
+              U_OU = "False";
+              UAN_UANG = "False";
+              C_CH = "False";
+              F_H = "False";
+              L_N = "False";
+              L_R = "False";
+              S_SH = "False";
+              Z_ZH = "False";
+              Correction = "None";
+            };
+          };
+        };
       };
 
       # Optional, but recommended if ~/.config/fcitx5/config is overriding this.
